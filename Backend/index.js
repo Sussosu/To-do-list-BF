5145fcc11d9f6f99ee3cecb70c991c45ec783fbf
Susej Cova <117864949+Sussosu@users.noreply.github.com>
2023-03-28 13:23:30 -0500

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = 3030;
+const app = express();
+
+const todoRoutes = require("./routes/todoRoutes");
+const connectionOptions = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true, useFindAndModify: false
+};
+
+
+app.use(express.json());
+mongoose.connect("mongodb://localhost/todolist"
+    ,
+    connectionOptions)
+    .then(() => console.log("Connected successfully"))
+    .catch((err) => console.error(err));
+
+
+app.use("/todo"
+    , todoRoutes);
+    
+app.listen(PORT, () => {
+    console.log("The server is listening on port " + PORT);
+});
